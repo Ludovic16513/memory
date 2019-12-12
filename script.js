4b969dc7e1f44d5fe3def2ff3c8b8ca5d7e971a9
Ludovic16513 <56023526+Ludovic16513@users.noreply.github.com>
2019-12-12 16:46:09 +0100

    1;1

---
+++ script.js
@@ -0,0 +1,57 @@
+
+var score = document.getElementById('score');
+var position = document.getElementsByClassName('carte');
+
+//score.appendChild(CreateImg).src = carteImg[6];
+var trouve = [];
+var found = [];
+var carteImg = ['carte1.png','carte1.png','carte2.png','carte2.png','carte3.png','carte3.png','carte4.png','carte4.png','carte5.png','carte5.png','carte6.png','carte6.png'];
+var click = 0;
+
+function shuffle(array) {
+    array.sort(() => Math.random() - 0.5);
+}
+
+shuffle(carteImg);
+
+for (let i = 0; i < position.length; i++) {
+
+    position[i].addEventListener('click', function () {
+        click += 1;
+        this.src = carteImg[i];
+        trouve.push(carteImg[i]);
+        comparaison(i);
+    });
+
+}
+
+
+function comparaison(i) {
+    if (trouve[0] === trouve[1]) {
+        alert('trouvé');
+        found.push(trouve[0], trouve[1]);
+        trouve = [];
+        console.log(found)
+    }
+
+
+    for (let i = 0; i < found.length; i++)
+    {
+        if (carteImg != found)
+        {
+            position[i].src = 'doscarte.png';
+            console.log(found)
+        }
+
+    }
+
+
+    /*
+    if (click === 2 ) {
+        trouve = [];
+        click = 0;
+        alert('OO');
+    }
+
+
+}
